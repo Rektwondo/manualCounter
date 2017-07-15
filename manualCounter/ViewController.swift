56e037f64df9911c748fc4a474162ba08b3b517c
Adam Sokolowski <AdamSokolowski@MacBook-Pro-Adam.local>
2017-07-15 19:28:07 +0200

    Initial Commit

---
+++ manualCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  manualCounter
+//
+//  Created by Adam Sokolowski on 15/07/2017.
+//  Copyright © 2017 Adam Sokolowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
