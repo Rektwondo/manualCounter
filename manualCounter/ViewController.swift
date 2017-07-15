31174244d7af43536f297b71d359a94b0efbe52e
Adam Sokołowski <condsgn@gmail.com>
2017-07-15 22:31:12 +0200

    2 simple changes

--- manualCounter/ViewController.swift
+++ manualCounter/ViewController.swift
@@ -10,6 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    
+    var time = 100
+    
+    func decreaseTimer() {
+        
+        if time > 0 {
+            time -= 1
+            timerLabel.text = String(time)
+        
+        } else {
+            timer.invalidate()
+        }
+    }
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBAction func play(_ sender: Any) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+    }
+    @IBAction func pause(_ sender: Any) {
+        
+        timer.invalidate()
+    }
+    @IBAction func plusTen(_ sender: Any) {
+        
+        time += 10
+        timerLabel.text = String(time)
+    }
+    @IBAction func minusTen(_ sender: Any) {
+        
+        if time > 10 {
+        
+        time -= 10
+        timerLabel.text = String(time)
+            
+        }
+    }
+    @IBAction func resetTimer(_ sender: Any) {
+        
+        time = 100
+        timerLabel.text = String(time)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
